7bac6831dd8558857ee821f2a550e4e13c5015d3
Farquhar13 <farquharc356@strose.edu>
2018-04-16 16:03:12 -0400

    added mutation added scaling started weighting

--- GA.py
+++ GA.py
@@ -9,6 +9,8 @@ class Genetic:
     def __init__(self):
         self.c = self.generate_population()
         self.f = self.fitness_function()
+        self.scaled = None
+        self.weighted = None
         chromosomes.append(self)
 
     def generate_population(self):
@@ -19,16 +21,25 @@ class Genetic:
 
     def fitness_function(self):
         x = self.c
-        fitness = 4 * x[0] ** 2 - 2 * x[1] ** 3 + 9 * x[2] ** 2 - 11 * x[3] ** 2 + 5 * x[4] ** 0.5
-        + (x[5] + x[6]) ** 3 - 5 * x[7] ** 2 + 10 * (x[8] - x[9]) ** 2
+        fitness = 4*x[0]**2 - 2*x[1]**3 + 9*x[2]**2 - 11*x[3]**2 + 5*x[4]**0.5
+        + (x[5] + x[6])**3 - 5*x[7]**2 + 10*(x[8] - x[9])**2
         return fitness
 
-    #unfinished mutation method
+    # mutation method
     def mutate(self):
-        for x in self.c:
+        for i in range(10):
+            x = self.c[i]
             r = random.randint(1, 100)
             if r == 1:
-                pass
+                if i == 9:
+                    y = self.c[0]
+                    self.c[i] = y
+                    self.c[0] = x
+
+                y = self.c[i+1]
+                self.c[i] = y
+                self.c[i+1] = x
+
 
     # performs cross over and checks for duplicate numbers
     def switch_and_check(self, chrom, sub, cross_point):
@@ -48,7 +59,7 @@ class Genetic:
         return new_chrom
 
 
-    # could probably do separate get cross_point and crossover methods
+    # remember to only call this 80% of the time in the looping method
     def crossover(self, other_obj):
         chrom_one = self.c
         chrom_two = other_obj.c
@@ -75,10 +86,42 @@ class Genetic:
                     chromosomes.remove(c)
             f_list.remove(lowest)
 
+    def scaling(self):
+        f_list = [c.f for c in chromosomes]
+        min_f = min(f_list)
+
+        if min_f >= 0:
+            for c in chromosomes:
+                c.scaled = c.f
+
+        if min_f < 0:
+            for c in chromosomes:
+                c.scaled = c.f + abs(min_f) + 100
+
+    def weighting(self):
+
+
     def roulette(self):
-        total_f = 0
+        self.scaling()
+        self.weighted()
+        print "scaled"
         for c in chromosomes:
-            total_f += c.f
+            print c.scaled, ", ",
+        print
+
+        scaled_total = 0
+        scaled_list = [c.scaled for c in chromosomes]
+        for x in scaled_list:
+            scaled_total += x
+
+        weighted_total = 0
+        for c in chromosomes:
+            c.weighted = c.scaled / scaled_total
+            weighted_total += c.weighted
+
+        print "summing c.weighted"
+        print weighted_total
+
 
 
 
@@ -88,6 +131,8 @@ class Offspring(Genetic):
     def __init__(self, chromosome):
         self.c = chromosome
         self.f = self.fitness_function()
+        self.scaled = None
+        self.weighted = None
         chromosomes.append(self)
 
 
@@ -103,6 +148,8 @@ for c in chromosomes:
     print c.c
     print c.f
 
+starter.roulette()
+
 starter.find_lowest_two()
 
 print "after find lowest"
@@ -110,7 +157,7 @@ print "after find lowest"
 for c in chromosomes:
     print c.c
     print c.f
-
+    print c.scaled
 
 
 
--- test.py
+++ test.py
@@ -5,3 +5,7 @@ list = [4,2,2,3]
 print min(list)
 list.remove(2)
 print list
+
+a = None
+
+print a
